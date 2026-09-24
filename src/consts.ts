ef4a952c112275c36f0ffb33d3dbf25ed0922313
devYranzo <enekoy21@gmail.com>
2026-09-24 10:55:36 +0200

    Basic SEO

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,4 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'DevYranzo\'s Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "DevYranzo's Blog";
+export const SITE_DESCRIPTION = "Blog de Eneko Yranzo sobre desarrollo de software, herramientas, arquitectura y tecnología.";
+export const SITE_URL = "https://blog.enekoyranzo.dev";
+export const SITE_AUTHOR = "Eneko Yranzo";
